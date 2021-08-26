2fca808d532c1493eded17c1f8816d0d8584746d
Anton Rochniak <anton@izz.ai>
2021-08-26 12:35:48 +0300

    Initial assesment commit

---
+++ src/components/List.tsx
@@ -0,0 +1,13 @@
+import { ListItem } from 'mocks'
+
+export const List = ({ items }: ListProps) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+type ListProps = {
+  items: ListItem[]
+}
---
+++ src/mocks/index.ts
@@ -0,0 +1,42 @@
+import { shuffle, pause } from 'utils'
+
+
+export const loadUser = async (): Promise<IUserInfo> => {
+  await pause()
+  return {
+    firstname: 'Foo',
+    lastname: 'Bar',
+    age: 30
+  }
+}
+
+const colors = [
+  '#396AB1',
+  '#DA7C30',
+  '#3E9651',
+  '#CC2529',
+  '#535154',
+  '#6B4C9A',
+  '#922428',
+  '#948B3D',
+]
+
+export const loadList = async (): Promise<ListItem[]> => {
+  await pause()
+  const list = new Array(1000).fill(0).map((_, idx) => ({
+    number: idx + 1,
+    color: colors[idx % colors.length]
+  }))
+  return shuffle(list)
+}
+
+export type IUserInfo = {
+  firstname?: string
+  lastname: string
+  age: number
+}
+
+export type ListItem = {
+  number: number
+  color: string
+}
